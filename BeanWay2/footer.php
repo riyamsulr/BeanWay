e7c50b8d79bf8c9b4e90ec9be0963d1dd40772eb
raghadzworld <raghad_2004@outlook.sa>
2025-12-01 08:01:05 +0300

    Delete BeanWay2 directory

--- BeanWay2/footer.php
+++
@@ -1,7 +0,0 @@
-<footer style="background-color:#0E3D34; color:#F4F1EC; text-align:center; padding:20px; margin-top:50px;">
-    <div class="copyright">
-      © 2025 BeanWay Coffee. All Rights Reserved.
-    </div>
-  </footer>
-</body>
-</html>
